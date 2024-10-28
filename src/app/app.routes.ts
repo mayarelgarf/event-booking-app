4c0671163c0e17dec23972e6e9016b4a73a8096b
mayarelgarf <mayarmahmoud123@gmail.com>
2024-10-28 20:26:14 +0300

    add home component and styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { EventFormComponent } from './event-form/event-form.component';
+import { mainPath } from './enums/main-path.enums';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  {path:'', component:EventFormComponent}
+  {path:'', redirectTo:mainPath.HOME,pathMatch:'full'},
+  {path: mainPath.HOME,component:HomeComponent},
+  {path:mainPath.EVENT_FORM, component:EventFormComponent}
 ];
