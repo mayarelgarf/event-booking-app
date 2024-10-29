553c26f62d3ef11046fa4fd0b26f358b1956e9c5
mayarelgarf <mayarmahmoud123@gmail.com>
2024-10-29 17:17:32 +0300

    adjust error message  style

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { EventFormComponent } from './event-form/event-form.component';
 import { mainPath } from './enums/main-path.enums';
 import { HomeComponent } from './home/home.component';
+import { SuccessPageComponent } from './success-page/success-page.component';
 
 export const routes: Routes = [
   {path:'', redirectTo:mainPath.HOME,pathMatch:'full'},
   {path: mainPath.HOME,component:HomeComponent},
-  {path:mainPath.EVENT_FORM, component:EventFormComponent}
+  {path:mainPath.EVENT_FORM, component:EventFormComponent},
+  {path:mainPath.SUCCESS_PAGE, component:SuccessPageComponent},
+  {path:'**', component:HomeComponent}
 ];
--- src/app/event-form/event-form.component.ts
+++ src/app/event-form/event-form.component.ts
@@ -1,52 +1,40 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { eventsForm } from '../interfaces/events.interface';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { Router } from '@angular/router';
+import { mainPath } from '../enums/main-path.enums';
+import { Subject, takeUntil } from 'rxjs';
 @Component({
   selector: 'app-event-form',
   standalone: true,
-  imports: [ReactiveFormsModule,MatFormFieldModule,MatInputModule,MatCheckboxModule,MatButtonModule,MatCardModule],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatCardModule,
+  ],
   templateUrl: './event-form.component.html',
-  styleUrls: ['./event-form.component.scss']
+  styleUrls: ['./event-form.component.scss'],
 })
-export class EventFormComponent {
+export class EventFormComponent implements OnInit {
+  _destroy$ = new Subject<void>();
   eventForm: FormGroup;
   undoStack: eventsForm[] = [];
   redoStack: eventsForm[] = [];
-  constructor(private _fb: FormBuilder) {
-    this.eventForm = this._fb.group({
-      name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      acceptTerms: [false],
-
-    });
-  }
-  ngOnInit() {
-    // Save initial state
-    this.pushToUndoStack();
-
-    // Track changes
-    this.eventForm.valueChanges.subscribe(() => {
-      if(this.redoStack.length >1){
-        this.redoStack = []; // Clear redo stack on new changes
-      }
-
-      this.pushToUndoStack();
-    });
-  }
-  pushToUndoStack() {
-
-    const eventsForm: eventsForm = { ...this.eventForm.value };
-
-
-    console.log('pushing to undo stack',eventsForm)
-    this.undoStack.push(eventsForm);
-
-  }
+  isSubmitted: boolean = false;
+  errorMessages: string[] = [];
   get canUndo(): boolean {
     return this.undoStack.length > 1; // Enable if there’s something to undo beyond the initial state
   }
@@ -54,15 +42,41 @@ export class EventFormComponent {
   get canRedo(): boolean {
     return this.redoStack.length > 0;
   }
+  constructor(private _fb: FormBuilder, private _router: Router) {
+    this.eventForm = this._fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      acceptTerms: [false],
+    });
+  }
+  ngOnInit() {
+    this.handleFormChanges();
+  }
+  handleFormChanges() {
+    this.pushToUndoStack();
+    this.eventForm.valueChanges
+      .pipe(takeUntil(this._destroy$))
+      .subscribe(() => {
+        if (this.redoStack.length > 1) {
+          this.redoStack = []; // Clear redo stack on new changes
+        }
+
+        this.pushToUndoStack();
+      });
+  }
+  pushToUndoStack() {
+    const eventsForm: eventsForm = { ...this.eventForm.value };
+    this.undoStack.push(eventsForm);
+  }
+
 
   undo() {
-    if (this.undoStack.length > 1) { // Prevent undo beyond initial state
+    if (this.undoStack.length > 1) {
+      // Prevent undo beyond initial state
       const currentState = this.undoStack.pop()!;
-      console.log('item poped',currentState)
       this.redoStack.push(currentState);
       const previousState = this.undoStack[this.undoStack.length - 1];
-      console.log('previous state',previousState)
-      this.eventForm.setValue(previousState,{emitEvent:false});
+      this.eventForm.setValue(previousState, { emitEvent: false });
     }
   }
 
@@ -73,6 +87,37 @@ export class EventFormComponent {
       this.eventForm.setValue(redoState);
     }
   }
+  getErrorMessages(): string[] {
+    const messages: string[] = [];
 
+    if (this.eventForm.controls['name'].errors) {
+      if (this.eventForm.controls['name'].errors['required']) {
+        messages.push('Name is required.');
+      }
+    }
+
+    if (this.eventForm.controls['email'].errors) {
+      if (this.eventForm.controls['email'].errors['required']) {
+        messages.push('Email is required.');
+      }
+      if (this.eventForm.controls['email'].errors['email']) {
+        messages.push('Please enter a valid email address.');
+      }
+    }
+
+    return messages;
+  }
+  onSubmit() {
+    if (this.eventForm.invalid) {
+      this.errorMessages = this.getErrorMessages();
+      this.isSubmitted = true;
+    } else {
+      this.isSubmitted = false;
+      this._router.navigate([mainPath.SUCCESS_PAGE]);
+    }
+  }
+  ngOnDestroy() {
+    this._destroy$.next();
+    this._destroy$.complete();
+  }
 }
-
